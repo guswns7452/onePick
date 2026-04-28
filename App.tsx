dadbb606ff890c738b9c37b44f671615cb1dff3a
yoon349 <j_yo.on_e@sookmyung.ac.kr>
2026-04-28 17:03:59 +0900

    Setting

--- App.tsx
+++ App.tsx
@@ -5,12 +5,46 @@
  * @format
  */
 
+
+
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <View style={styles.box}>
+        <Text style={styles.text}>👋 OnePick 👋</Text>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  box: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingTop: 200,
+  },
+  text: {
+    fontSize: 36,
+  }
+});
+
+
+/*
+
 import { NewAppScreen } from '@react-native/new-app-screen';
-import { StatusBar, StyleSheet, useColorScheme, View } from 'react-native';
+import React from 'react';
+import { StatusBar, StyleSheet, Text, useColorScheme, View } from 'react-native';
 import {
   SafeAreaProvider,
   useSafeAreaInsets,
 } from 'react-native-safe-area-context';
+
 
 function App() {
   const isDarkMode = useColorScheme() === 'dark';
@@ -32,6 +66,9 @@ function AppContent() {
         templateFileName="App.tsx"
         safeAreaInsets={safeAreaInsets}
       />
+      <View style={styles.box}>
+        <Text style={styles.text}>HELLO, SORI</Text>
+      </View>
     </View>
   );
 }
@@ -40,6 +77,13 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
   },
+  box: {
+    alignSelf: 'center',
+  },
+  text: {
+    fontSize: 36,
+  }
 });
 
 export default App;
+*/
